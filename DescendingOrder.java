ccb2d708ed15e42309060c4ed47a3618a0a5d9fd
Jeremi Frazier <jerm_803@yahoo.com>
2016-05-13 15:17:31 -0400

    Update DescendingOrder.java

--- DescendingOrder.java
+++ DescendingOrder.java
@@ -1,7 +1,19 @@
 import java.lang.StringBuilder;
 import java.util.Arrays;
 
+/**
+ * A class that allows you to sort integers into descending order
+ * @author Jeremi Frazier
+ * @version 1.2
+ */
+
 public class DescendingOrder {
+  
+  /**
+   * This method sorts an integer into descending order and return the sorted integer
+   * @param num - the number to sort i.e 9134 = 9431
+   * @return int - the sorted (DESC) number
+   */
   public static int sortDesc(final int num) {
   char[] charString = Integer.toString(num).toCharArray();
   Arrays.sort(charString);
